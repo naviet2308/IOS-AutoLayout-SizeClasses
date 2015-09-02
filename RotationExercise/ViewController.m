28b9abd81be771ffdd154f3e50fdd5c7d7234bea
NAV <naviet2308@gmail.com>
2015-09-02 10:40:34 +0700

    upload portrait screen

---
+++ RotationExercise/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RotationExercise
+//
+//  Created by Nguyen Anh Viet on 9/1/15.
+//  Copyright (c) 2015 NAV. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
